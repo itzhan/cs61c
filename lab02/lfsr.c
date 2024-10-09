0f85897b43f9a1bfc0b4446a32898624f7b268d7
itzhan <2802965114@qq.com>
2024-10-09 11:49:45 +0800

    lfsr done

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,6 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    int msb = (*reg >> 0 & 1) ^ (*reg >> 2 & 1) ^ (*reg >> 3 & 1) ^ (*reg >> 5 & 1);
+    *reg = (((*reg >> 1) & ~(1 << 15)) | (msb << 15));
 }
-
