986f853eea55de073ee1f756335432cd4d18ad3a
itzhan <2802965114@qq.com>
2024-10-12 10:30:09 +0800

    lab03--list_map done

--- lab03/list_map.s
+++ lab03/list_map.s
@@ -17,6 +17,7 @@ main:
 
     # load the address of the function in question into a1 (check out la on the green sheet)
     ### YOUR CODE HERE ###
+    la a1, square
 
     # issue the call to map
     jal ra, map
@@ -33,8 +34,15 @@ main:
 
 map:
     # Prologue: Make space on the stack and back-up registers
+    # ra, a0, s0, s1  使用了这四个寄存器
     ### YOUR CODE HERE ###
+    addi sp, sp, -16
+    sw ra, 0(sp)
+    sw a0, 4(sp)
+    sw s0, 8(sp)
+    sw s1, 12(sp)
 
+map_loop:
     beq a0, x0, done    # If we were given a null pointer (address 0), we're done.
 
     add s0, a0, x0  # Save address of this node in s0
@@ -46,30 +54,41 @@ map:
     # load the value of the current node into a0
     # THINK: why a0?
     ### YOUR CODE HERE ###
+    lw a0, 0(s0)
 
     # Call the function in question on that value. DO NOT use a label (be prepared to answer why).
     # What function? Recall the parameters of "map"
     ### YOUR CODE HERE ###
+    jalr a1
 
     # store the returned value back into the node
-    # Where can you assume the returned value is?
+    # Where can you assume the returned value is?  a0
     ### YOUR CODE HERE ###
+    sw a0, 0(s0)
 
     # Load the address of the next node into a0
     # The Address of the next node is an attribute of the current node.
     # Think about how structs are organized in memory.
     ### YOUR CODE HERE ###
+    lw a0, 4(s0)
 
     # Put the address of the function back into a1 to prepare for the recursion
     # THINK: why a1? What about a0?
     ### YOUR CODE HERE ###
+    add a1, s1, x0
 
     # recurse
     ### YOUR CODE HERE ###
+    j map_loop
 
 done:
     # Epilogue: Restore register values and free space from the stack
     ### YOUR CODE HERE ###
+    lw ra, 0(sp)
+    lw a0, 4(sp)
+    lw s0, 8(sp)
+    lw s1, 12(sp)
+    addi sp, sp, 16
 
     jr ra # Return to caller
 
@@ -90,7 +109,7 @@ loop:   #do...
     sw  s1, 0(a0)   # node->value = i
     sw  s0, 4(a0)   # node->next = last
     add s0, a0, x0  # last = node
-    addi    s1, s1, 1   # i++
+    addi s1, s1, 1   # i++
     addi t0, x0, 10
     bne s1, t0, loop    # ... while i!= 10
     lw  ra, 0(sp)
@@ -121,6 +140,6 @@ print_newline:
 
 malloc:
     addi    a1, a0, 0
-    addi    a0, x0 9
+    addi    a0, x0, 9   # 9是调用系统内存分配程序 a1是指定要分配的大小
     ecall
     jr  ra
