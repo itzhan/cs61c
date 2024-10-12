d58f1ff8bc7f16d56ba5fdf459361e84acd94474
itzhan <2802965114@qq.com>
2024-10-12 13:30:07 +0800

    lab04--megalistmanips done

--- lab04/megalistmanips.s
+++ lab04/megalistmanips.s
@@ -50,42 +50,48 @@ map:
     sw ra, 0(sp)
     sw s1, 4(sp)
     sw s0, 8(sp)
-
-    beq a0, x0, done    # if we were given a null pointer, we're done.
-
-    add s0, a0, x0      # save address of this node in s0
-    add s1, a1, x0      # save address of function in s1
-    add t0, x0, x0      # t0 is a counter
-
     # remember that each node is 12 bytes long:
     # - 4 for the array pointer
     # - 4 for the size of the array
     # - 4 more for the pointer to the next node
 
     # also keep in mind that we should not make ANY assumption on which registers
-    # are modified by the callees, even when we know the content inside the functions 
+    # are modified by the callees, even when we know the content inside the functions
     # we call. this is to enforce the abstraction barrier of calling convention.
-mapLoop:
-    add t1, s0, x0      # load the address of the array of current node into t1
+
+map_outLoop:
+    beq a0, x0, done    # if we were given a null pointer, we're done.
+
+    add s0, a0, x0      # save address of this node in s0
+    add s1, a1, x0      # save address of function in s1
+    add t0, x0, x0      # t0 is a counter
+
+    lw t1, 0(s0)        # load the address of the array of current node into t1
     lw t2, 4(s0)        # load the size of the node's array into t2
 
-    add t1, t1, t0      # offset the array address by the count
-    lw a0, 0(t1)        # load the value at that address into a0
+map_innerLoop:
+    slli t3, t0, 2      # t3 = t0 * 4
+    add t4, t3, t1      # t4 = current number address in array
+    lw a0, 0(t4)        # load the value at that address into a0
 
+    addi sp, sp, -4
+    sw t1, 0(sp)
     jalr s1             # call the function on that value.
+    lw t1, 0(sp)
+    addi sp, sp, 4
 
-    sw a0, 0(t1)        # store the returned value back into the array
+    sw a0, 0(t4)        # store the returned value back into the array
     addi t0, t0, 1      # increment the count
-    bne t0, t2, mapLoop # repeat if we haven't reached the array size yet
+    bne t0, t2, map_innerLoop # repeat if we haven't reached the array size yet
 
-    la a0, 8(s0)        # load the address of the next node into a0
-    lw a1, 0(s1)        # put the address of the function back into a1 to prepare for the recursion
+    lw a0, 8(s0)        # load the address of the next node into a0
+    add a1, s1, x0       # put the address of the function back into a1 to prepare for the recursion
 
-    jal  map            # recurse
+    jal  map_outLoop            # recurse
 done:
-    lw s0, 8(sp)
-    lw s1, 4(sp)
     lw ra, 0(sp)
+    lw s1, 4(sp)
+    lw s0, 8(sp)
     addi sp, sp, 12
     jr ra
 
